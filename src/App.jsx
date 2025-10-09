47e114fbb9c27ae45b5952041e339be05fd1ccd9
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-10 00:37:11 +0530

    Refactor API calls to use BASE_URL constant for improved maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import React from "react";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -14,10 +13,12 @@ import EditJobPage from "./pages/EditJobPage";
 import AddJobPage from "./pages/AddJobPage";
 import MainLayout from "./layouts/Mainlayout";
 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
 const App = () => {
   // Add new job
-const addJob = async (newJob) => {
-    const res = await fetch("https://react-job-website-backend.onrender.com/jobs", {
+  const addJob = async (newJob) => {
+    await fetch(`${BASE_URL}/jobs`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -28,16 +29,16 @@ const addJob = async (newJob) => {
   };
 
   // Delete job
-const deleteJob = async (id) => {
-    const res = await fetch(`https://react-job-website-backend.onrender.com/jobs/${id}`, {
-      method: "DELETE"
+  const deleteJob = async (id) => {
+    await fetch(`${BASE_URL}/jobs/${id}`, {
+      method: "DELETE",
     });
     return;
   };
 
   // Update Job
-const updatedJob = async (job) => {
-    const res = await fetch(`https://react-job-website-backend.onrender.com/jobs/${job.id}`, {
+  const updatedJob = async (job) => {
+    await fetch(`${BASE_URL}/jobs/${job.id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -55,7 +56,7 @@ const updatedJob = async (job) => {
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route
           path="/edit-job/:id"
-          element={<EditJobPage updatedJobSubmit={updatedJob}/>}
+          element={<EditJobPage updatedJobSubmit={updatedJob} />}
           loader={jobLoder}
         />
         <Route
@@ -71,4 +72,3 @@ const updatedJob = async (job) => {
 };
 
 export default App;
-
